4730b52c28f7324023ccac7065f18ea3dab78369
Alifram <aliframadhan27sept@gmail.com>
2024-05-11 21:02:16 +0700

    update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./about.css";
 import AboutImg from "../../assets/about.jpg";
-import CV from "../../assets/Davinda-Cv.pdf";
+import CV from "../../assets/Alif-CV.pdf";
 import Info from './Info';
 
 const About = () => {
